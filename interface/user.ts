2e4c88fbaec7f320be8bd527146f958482e315e1
StanislavMelnik1991 <StanislavMelnik@gmail.com>
2024-11-10 18:21:09 +0300

    update userInterface

--- interface/user.ts
+++ interface/user.ts
@@ -10,4 +10,8 @@ export type CreateUserType = z.infer<typeof createUserSchema>;
 export type RegistrationUserType = z.infer<typeof registrationUserSchema>;
 export type LoginUserType = z.infer<typeof loginUserSchema>;
 
-export type UserType = Omit<CreateUserType, 'password'> & IBaseEntity;
+export type UserType = Omit<CreateUserType, 'password'> &
+  IBaseEntity & {
+    imageId?: number;
+    image?: string;
+  };
