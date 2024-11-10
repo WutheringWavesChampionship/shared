ea6d9b299d74ec143617c40f11004a21e3fd32d5
StanislavMelnik1991 <StanislavMelnik@gmail.com>
2024-11-10 17:28:14 +0300

    update registration scheme

--- interface/user.ts
+++ interface/user.ts
@@ -1,8 +1,13 @@
 import { z } from 'zod';
-import { createUserSchema, loginUserSchema } from '../schemes';
+import {
+  createUserSchema,
+  loginUserSchema,
+  registrationUserSchema,
+} from '../schemes';
 import { IBaseEntity } from './baseEntity';
 
 export type CreateUserType = z.infer<typeof createUserSchema>;
+export type RegistrationUserType = z.infer<typeof registrationUserSchema>;
 export type LoginUserType = z.infer<typeof loginUserSchema>;
 
 export type UserType = Omit<CreateUserType, 'password'> & IBaseEntity;
--- schemes/user.ts
+++ schemes/user.ts
@@ -25,6 +25,26 @@ export const createUserSchema = z
     { message: FieldErrorEnum.REQUIRED, path: ['password'] },
   );
 
+export const registrationUserSchema = z
+  .object({
+    username: z
+      .string({ required_error: FieldErrorEnum.REQUIRED })
+      .min(1, FieldErrorEnum.REQUIRED)
+      .max(256, FieldErrorEnum.MAX_256),
+    password: z
+      .string({ required_error: FieldErrorEnum.REQUIRED })
+      .min(1, FieldErrorEnum.REQUIRED)
+      .max(256, FieldErrorEnum.MAX_256),
+    password_confirmation: z
+      .string({ required_error: FieldErrorEnum.REQUIRED })
+      .min(1, FieldErrorEnum.REQUIRED)
+      .max(256, FieldErrorEnum.MAX_256),
+  })
+  .refine(
+    ({ password, password_confirmation }) => password === password_confirmation,
+    { message: FieldErrorEnum.PASSWORD_MATCH, path: ['password_confirmation'] },
+  );
+
 export const loginUserSchema = z.object({
   username: z
     .string({ required_error: FieldErrorEnum.REQUIRED })
